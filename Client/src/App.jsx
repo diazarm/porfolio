a8e6084199aa276ffbf9a2d78bd7d7f9b6afaceb
diazarm <marceloardiaz@gmail.com>
2023-09-28 11:23:56 -0300

    structure client

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,14 +1,19 @@
-import './App.css'
-import Threex from "./components/Three/Threex"
-
+import './App.css';
+import { Routes, Route, useLocation } from "react-router-dom";
+import { Landing, Home } from "./views";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
-
+  const location = useLocation();
   return (
-    <div>
-      <Threex/>
-    </div>
-  )
+    <div className="App">
+      {location.pathname !== "/" && location.pathname !== "/checkin" && location.pathname !== "/home" &&<NavBar/>}
+      <Routes>
+        <Route exact path="/" element={<Landing />} />
+        <Route exact path="/home" element={<Home />} />
+      </Routes>
+   </div>
+  );
 }
 
-export default App
+export default App;
