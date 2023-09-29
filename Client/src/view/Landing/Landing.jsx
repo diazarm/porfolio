e260811df3d21703dccfeeccadda3c70cf3c8f98
diazarm <marceloardiaz@gmail.com>
2023-09-29 16:37:10 -0300

    update

--- Client/src/view/Landing/Landing.jsx
+++ Client/src/view/Landing/Landing.jsx
@@ -5,9 +5,12 @@ const Landing = () => {
   return (
     <div className={style.container}>
     <div className={style.box1}>
-
+    <div className={style.vid}>
+        <video loop autoPlay muted>
+          <source src="https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4" type="video/mp4" />
+          </video>
+        </div>
     <div className={style.box2}>
-      Landing
 
     </div>
     </div>
@@ -16,3 +19,4 @@ const Landing = () => {
 }
 
 export default Landing
+
