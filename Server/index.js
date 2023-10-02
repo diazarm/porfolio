ad16a7e718e5337ac6e00469ae743b25162b342d
diazarm <marceloardiaz@gmail.com>
2023-10-02 18:58:11 -0300

    del

--- Server/index.js
+++
@@ -1,9 +0,0 @@
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
-
-const PORT = 3001;
-
-server.listen(PORT, ()=> {
-  conn.sync({force:false}); //alter (es como refresh)
-  console.log(`Listening on port ${PORT}`);
-});
--- readme.md
+++
@@ -1,2 +0,0 @@
-<h1>Welcome to my porfolio </h1>
-<p>This is under construction</p>
